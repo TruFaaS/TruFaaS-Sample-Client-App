dd54852d9c13c2d311a6672ad09afefd7ea52727
sgayangi <gayangi.18@cse.mrt.ac.lk>
2023-04-25 19:57:56 +0530

    test body added

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
-package TruFaaSClientApp
+package main
 
 import (
 	"TruFaaSClientApp/constants"
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/hmac"
@@ -17,10 +18,66 @@ import (
 )
 
 func main() {
-	url := os.Args[3]
+	url := os.Args[1]
 	fmt.Println("Invoking function at URL ", url)
 
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	var jsonData = []byte(`{
+    "function_information": {
+        "function_name": "gs",
+        "function_namespace": "default",
+        "function_spec": {
+            "environment": {
+                "namespace": "default",
+                "name": "nodejs"
+            },
+            "package_ref": {
+                "namespace": "default",
+                "name": "hello-js-73844e1f-92fc-4132-9d30-bbfdf57c17cb",
+                "resource_version": "110732"
+            },
+            "invoke_strategy": {
+                "execution_strategy": {
+                    "executor-type": "poolmgr",
+                    "min_scale": 0,
+                    "max_scale": 10,
+                    "target_cpu_percent": 0,
+                    "specialization_timeout": 120
+                },
+                "strategy_type": "execution"
+            },
+            "function_timeout": 70,
+            "idle_timeout": 120,
+            "concurrency": 500,
+            "requests_per_pod": 1
+        }
+    },
+    "package_information": {
+        "package_name": "hello-js-73844e1f-92fc-4132-9d30-bbfdf57c17cb",
+        "package_namespace": "default",
+        "package_spec": {
+            "environment": {
+                "namespace": "default",
+                "name": "nodejs"
+            },
+            "source": {
+                "checksum": {
+                    "type": "",
+                    "sum": ""
+                }
+            },
+            "deployment": {
+                "type": "literal",
+                "literal": "Cm1vZHVsZS5leHBvcnRzID0gYXN5bmMgZnVuY3Rpb24oY29udGV4dCkgewogICAgcmV0dXJuIHsKICAgICAgICBzdGF0dXM6IDIwMCwKICAgICAgICBib2R5OiAiaGVsbG8sIHdvcmxkIVxuIgogICAgfTsKfQo=",
+                "checksum": {
+                    "type": "",
+                    "sum": ""
+                }
+            }
+        }
+    }
+}`)
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonData))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -36,7 +93,6 @@ func main() {
 	clientPubKeyHex := hex.EncodeToString(clientPubKeyBytes)
 
 	req.Header.Set(constants.ClientPublicKeyHeader, clientPubKeyHex)
-
 	// calling the function
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -53,22 +109,21 @@ func main() {
 	}
 	fmt.Println(string(body))
 
+	// Accessing the headers
 	// Get the server's public key
 	serverPublicKeyHex := resp.Header.Get(constants.ServerPublicKeyHeader)
+	// Get the MAC tag
+	macTag := resp.Header.Get(constants.MacHeader)
+	// Get the trust verification result
+	trustVerificationTag := resp.Header.Get(constants.TrustVerificationHeader)
+	// Get the function name
+	functionName := resp.Header.Get(constants.FunctionNameHeader)
 
 	//for key, values := range resp.Header {
 	//	for _, value := range values {
 	//		fmt.Printf("%s: %s\n", key, value)
 	//	}
 	//}
-
-	// Get the MAC tag
-	macTag := resp.Header.Get(constants.MacHeader)
-
-	// Get the trust verification result
-	trustVerificationTag := resp.Header.Get(constants.TrustVerificationHeader)
-
-	functionName := resp.Header.Get(constants.FunctionNameHeader)
 
 	if !verifyMacTag(serverPublicKeyHex, clientPrivKey, trustVerificationTag, functionName, macTag) {
 		fmt.Println("MAC tag verification failed")
@@ -88,7 +143,6 @@ func verifyMacTag(serverPubKeyHex string, clientPrivateKey *ecdsa.PrivateKey, tr
 		X:     new(big.Int).SetBytes(serverPubKeyBytes[:32]),
 		Y:     new(big.Int).SetBytes(serverPubKeyBytes[32:]),
 	}
-
 	sharedSecret, _ := serverPubKey.Curve.ScalarMult(serverPubKey.X, serverPubKey.Y, clientPrivateKey.D.Bytes())
 	fmt.Println(sharedSecret.Bytes())
 
