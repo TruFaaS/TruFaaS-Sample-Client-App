b81ee785736b4bd8e7291e06a033fb0d7873c94f
Avishka-Shamendra <avishka199849@gmail.com>
2023-04-29 15:04:50 +0530

    Contd. performance eval script

--- performance-eval/script.go
+++ performance-eval/script.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -9,15 +11,27 @@ import (
 func main() {
 
 	// Port forwarding must be manually run --> kubectl port-forward svc/router 31314:80 -n fission
-	deployFunction()
-	invokeFunction()
-	deleteFunction()
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Access the variables from the .env file
+	fnEnv := os.Getenv("FN_ENV")
+	fnName := os.Getenv("FN_NAME")
+	fnFile := os.Getenv("FN_FILE")
+	fnFileType := os.Getenv("FN_FILE_TYPE")
+
+	deployFunction(fnName, fnEnv, fnFile, fnFileType)
+	invokeFunction(fnName)
+	deleteFunction(fnName)
 
 }
 
-func deployFunction() {
+func deployFunction(fnName string, fnEnv string, fnFile string, fnFileType string) {
 	// Command 1: fission fn create --name test --env nodejs --code hello.js
-	cmd1 := exec.Command("fission", "fn", "create", "--name", "test", "--env", "nodejs", "--code", "./functions/hello.js")
+	cmd1 := exec.Command("fission", "fn", "create", "--name", fnName, "--env", fnEnv, fnFileType, fnFile)
 	cmd1.Dir = "."
 	cmd1.Stdout = os.Stdout
 	cmd1.Stderr = os.Stderr
@@ -27,7 +41,7 @@ func deployFunction() {
 	}
 
 	// Command 2: fission route create --name test --function test --url test
-	cmd2 := exec.Command("fission", "route", "create", "--name", "test", "--function", "test", "--url", "test")
+	cmd2 := exec.Command("fission", "route", "create", "--name", fnName, "--function", fnName, "--url", fnName)
 	cmd2.Dir = "."
 	cmd2.Stdout = os.Stdout
 	cmd2.Stderr = os.Stderr
@@ -37,9 +51,9 @@ func deployFunction() {
 	}
 }
 
-func invokeFunction() {
+func invokeFunction(fnName string) {
 	// Command: curl http://localhost:31314/test
-	cmd := exec.Command("curl", "http://localhost:31314/test")
+	cmd := exec.Command("curl", "http://localhost:31314/"+fnName)
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Println("error invoking:", err)
@@ -48,9 +62,9 @@ func invokeFunction() {
 	fmt.Println("function invoked. result:", string(output))
 }
 
-func deleteFunction() {
+func deleteFunction(fnName string) {
 	// Command 1: fission fn delete --name test
-	cmd1 := exec.Command("fission", "fn", "delete", "--name", "test")
+	cmd1 := exec.Command("fission", "fn", "delete", "--name", fnName)
 	cmd1.Dir = "."
 	cmd1.Stdout = os.Stdout
 	cmd1.Stderr = os.Stderr
@@ -59,13 +73,23 @@ func deleteFunction() {
 		panic(err1)
 	}
 
-	// Command 2: fission delete create --name test
-	cmd2 := exec.Command("fission", "route", "delete", "--name", "test")
+	// Command 2: fission route delete --name test
+	cmd2 := exec.Command("fission", "route", "delete", "--name", fnName)
 	cmd2.Dir = "."
 	cmd2.Stdout = os.Stdout
 	cmd2.Stderr = os.Stderr
 	err2 := cmd2.Run()
 	if err2 != nil {
+		panic(err2)
+	}
+
+	// Command 2: fission pkg delete --orphan=true
+	cmd3 := exec.Command("fission", "pkg", "delete", "--orphan=true")
+	cmd3.Dir = "."
+	cmd3.Stdout = os.Stdout
+	cmd3.Stderr = os.Stderr
+	err3 := cmd3.Run()
+	if err3 != nil {
 		panic(err2)
 	}
 }
